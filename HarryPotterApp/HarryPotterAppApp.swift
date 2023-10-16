63ca6f597a6d917f36ec3494fb8df56a15a2fbd3
Alejandra SM <alejandrasanmartin2911@gmail.com>
2023-10-16 15:19:12 -0500

    Initial Commit

---
+++ HarryPotterApp/HarryPotterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterAppApp.swift
+//  HarryPotterApp
+//
+//  Created by Alejandra San Martin on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
